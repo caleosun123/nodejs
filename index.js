b9c79f1d913b222c8a00dde147841a279a2a7a5b
caleosun123 <osunagarciacaleb@gmail.com>
2025-03-20 08:20:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require("hbs");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const path = require('path');
 const router = require('./pages/pages.js');
 
 const app = express();
@@ -11,6 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(router);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.js"));
+})
 
 app.listen(8080, () => {
     console.log("Listening on port 8080!")
